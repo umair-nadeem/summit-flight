e9b187f8a1daeb3212672b47e39e73084d56a7d4
Umair Nadeem <umair.cn@outlook.com>
2025-09-05 22:36:30 +0100

    refactor i2c into cpp file

--- common/stm/hw/include/hw/i2c/I2c.hpp
+++ common/stm/hw/include/hw/i2c/I2c.hpp
@@ -15,322 +15,38 @@ class I2c
    using ReceiveCompleteCallback = void (*)(void*);
 
 public:
-   explicit I2c(I2cConfig& config)
-       : m_i2c_config{config}
-   {
-   }
+   explicit I2c(I2cConfig& config);
 
-   void prepare_for_communication()
-   {
-      while ((LL_I2C_IsActiveFlag_STOP(m_i2c_config.i2c_handle) == 1u) ||
-             (LL_I2C_IsActiveFlag_ARLO(m_i2c_config.i2c_handle) == 1u))   // Loop until end of transfer received
-      {
-         LL_I2C_Disable(m_i2c_config.i2c_handle);
-         clear_status();
-      }
+   void prepare_for_communication();
 
-      while (LL_I2C_IsEnabled(m_i2c_config.i2c_handle) == 0u)
-      {
-         LL_I2C_Enable(m_i2c_config.i2c_handle);
-      }
-
-      m_state = I2cState::idle;
-   }
-
-   bool attempt_reinit()
-   {
-      bool success = false;
-
-      if ((LL_I2C_IsActiveFlag_STOP(m_i2c_config.i2c_handle) == 1u) ||
-          (LL_I2C_IsActiveFlag_ARLO(m_i2c_config.i2c_handle) == 1u))
-      {
-         LL_I2C_Disable(m_i2c_config.i2c_handle);
-         clear_status();
-      }
-
-      if (LL_I2C_IsEnabled(m_i2c_config.i2c_handle) == 0u)
-      {
-         LL_I2C_Enable(m_i2c_config.i2c_handle);
-         success = true;
-         m_state = I2cState::idle;
-      }
-
-      return success;
-   }
+   bool attempt_reinit();
 
    // Start write transaction (non-blocking)
-   bool write(const uint8_t i2c_addr, std::span<const uint8_t> tx_buffer)
-   {
-      error::verify(tx_buffer.size() <= max_bytes_per_i2c_transfer);
-
-      if (m_state == I2cState::idle)
-      {
-         m_current_i2c_addr = i2c_addr;
-         m_tx_buffer        = tx_buffer;
-         m_rx_buffer        = {};
-         m_read_reg         = std::nullopt;
-         m_tx_index         = 0;
-         m_rx_index         = 0;
-
-         enable_interrupts();
-
-         m_read_transaction.store(false, std::memory_order_relaxed);
-         m_state = I2cState::start_condition_for_data;
-
-         // generate start
-         LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);
-
-         return true;
-      }
-
-      return false;
-   }
+   bool write(const uint8_t i2c_addr, std::span<const uint8_t> tx_buffer);
 
    // Start read transaction (non-blocking)
-   bool read(const uint8_t i2c_addr, std::span<uint8_t> rx_buffer, std::optional<uint8_t> reg)
-   {
-      error::verify(rx_buffer.size() <= max_bytes_per_i2c_transfer);
+   bool read(const uint8_t i2c_addr, std::span<uint8_t> rx_buffer, std::optional<uint8_t> reg);
 
-      if (m_state == I2cState::idle)
-      {
-         m_current_i2c_addr = i2c_addr;
-         m_tx_buffer        = {};
-         m_rx_buffer        = rx_buffer;
-         m_read_reg         = reg;
-         m_tx_index         = 0;
-         m_rx_index         = 0;
+   void handle_i2c_event_interrupt();
 
-         enable_interrupts();
+   void handle_i2c_error_interrupt();
 
-         m_read_transaction.store(true, std::memory_order_relaxed);
+   bool has_error() const;
 
-         if (m_read_reg.has_value())
-         {
-            m_state = I2cState::start_condition_for_read_register;
-         }
-         else
-         {
-            m_state = I2cState::start_condition_for_data;
-         }
-
-         // generate start
-         LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);
-         return true;
-      }
-
-      return false;
-   }
-
-   void handle_i2c_event_interrupt()
-   {
-      const auto state            = m_state;
-      const auto read_transaction = m_read_transaction.load(std::memory_order_relaxed);
-
-      switch (state)
-      {
-         case I2cState::idle:
-         case I2cState::error:
-            // do nothing
-            break;
-
-         case I2cState::start_condition_for_read_register:
-            if (LL_I2C_IsActiveFlag_SB(m_i2c_config.i2c_handle) == 1u)
-            {
-               LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) & ~0x01);
-               m_state = I2cState::addr_flag_wait_for_read_register;
-            }
-            break;
-
-         case I2cState::addr_flag_wait_for_read_register:
-            if (LL_I2C_IsActiveFlag_ADDR(m_i2c_config.i2c_handle) == 1u)
-            {
-               LL_I2C_ClearFlag_ADDR(m_i2c_config.i2c_handle);
-               m_state = I2cState::send_read_register;
-            }
-            break;
-
-         case I2cState::send_read_register:
-            if (LL_I2C_IsActiveFlag_TXE(m_i2c_config.i2c_handle) == 1u)
-            {
-               LL_I2C_TransmitData8(m_i2c_config.i2c_handle, m_read_reg.value());
-               LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);   // re-generate start condition
-               m_state = I2cState::start_condition_for_data;
-            }
-            break;
-
-         case I2cState::start_condition_for_data:
-            if (LL_I2C_IsActiveFlag_SB(m_i2c_config.i2c_handle) == 1u)
-            {
-               if (read_transaction)
-               {
-                  LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) | 0x01);
-               }
-               else
-               {
-                  LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) & ~0x01);
-               }
-
-               m_state = I2cState::addr_flag_wait_for_data;
-            }
-            break;
-
-         case I2cState::addr_flag_wait_for_data:
-            if (LL_I2C_IsActiveFlag_ADDR(m_i2c_config.i2c_handle) == 1u)
-            {
-               LL_I2C_ClearFlag_ADDR(m_i2c_config.i2c_handle);
-
-               if (read_transaction)
-               {
-                  // for single-byte read, NACK is made before ADDR is cleared
-                  if (m_rx_buffer.size() == 1u)
-                  {
-                     LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_NACK);
-                     LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
-                  }
-                  else
-                  {
-                     LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_ACK);
-                  }
-               }
-               m_state = I2cState::data_transaction;
-            }
-            break;
-
-         case I2cState::data_transaction:
-            if (read_transaction)
-            {
-               const auto rx_length = m_rx_buffer.size();
-               if (m_rx_index < rx_length)
-               {
-                  receive_byte();
-                  // the ACK bit must be cleared just after reading the second last data byte
-                  if (m_rx_index == rx_length - 1u)
-                  {
-                     LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_NACK);
-                     LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
-                  }
-                  else
-                  {
-                     LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_ACK);
-                  }
-               }
-
-               if (m_rx_index == rx_length)
-               {
-                  stop(true);   // rx data read complete
-
-                  if (m_receive_complete_callback != nullptr)
-                  {
-                     m_receive_complete_callback(m_callback_context);
-                  }
-               }
-            }
-            else
-            {
-               // write
-               const auto tx_length = m_tx_buffer.size();
-               if (m_tx_index < tx_length)
-               {
-                  send_byte();
-               }
-
-               if (m_tx_index == tx_length)
-               {
-                  if (LL_I2C_IsActiveFlag_BTF(m_i2c_config.i2c_handle))
-                  {
-                     stop(true);   // tx data write complete
-                  }
-               }
-            }
-            break;
-
-         default:
-            error::stop_operation();
-            break;
-      }
-   }
-
-   void handle_i2c_error_interrupt()
-   {
-      stop(false);
-   }
-
-   bool has_error() const
-   {
-      return (m_state == I2cState::error);
-   }
-
-   void register_receive_complete_callback(const ReceiveCompleteCallback callback, void* context)
-   {
-      error::verify(callback != nullptr);
-      m_receive_complete_callback = callback;
-      m_callback_context          = context;
-   }
+   void register_receive_complete_callback(const ReceiveCompleteCallback callback, void* context);
 
 private:
-   void stop(const bool success)
-   {
-      LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
-      clear_status();
+   void stop(const bool success);
 
-      // clear any spurious byte lingering around
-      if (LL_I2C_IsActiveFlag_RXNE(m_i2c_config.i2c_handle) == 1u)
-      {
-         [[maybe_unused]] auto _ = LL_I2C_ReceiveData8(m_i2c_config.i2c_handle);
-      }
+   void receive_byte();
 
-      disable_interrupts();
+   void send_byte();
 
-      if (success)
-      {
-         m_state = I2cState::idle;
-      }
-      else
-      {
-         m_state = I2cState::error;
-      }
-   }
+   void enable_interrupts();
 
-   void receive_byte()
-   {
-      if (LL_I2C_IsActiveFlag_RXNE(m_i2c_config.i2c_handle) == 1u)
-      {
-         m_rx_buffer[m_rx_index] = LL_I2C_ReceiveData8(m_i2c_config.i2c_handle);
-         ++m_rx_index;
-      }
-   }
+   void disable_interrupts();
 
-   void send_byte()
-   {
-      if (LL_I2C_IsActiveFlag_TXE(m_i2c_config.i2c_handle) == 1u)
-      {
-         LL_I2C_TransmitData8(m_i2c_config.i2c_handle, m_tx_buffer[m_tx_index]);
-         ++m_tx_index;
-      }
-   }
-
-   void enable_interrupts()
-   {
-      LL_I2C_EnableIT_EVT(m_i2c_config.i2c_handle);
-      LL_I2C_EnableIT_ERR(m_i2c_config.i2c_handle);
-      LL_I2C_EnableIT_BUF(m_i2c_config.i2c_handle);
-   }
-
-   void disable_interrupts()
-   {
-      LL_I2C_DisableIT_EVT(m_i2c_config.i2c_handle);
-      LL_I2C_DisableIT_ERR(m_i2c_config.i2c_handle);
-      LL_I2C_DisableIT_BUF(m_i2c_config.i2c_handle);
-   }
-
-   void clear_status()
-   {
-      LL_I2C_ClearFlag_STOP(m_i2c_config.i2c_handle);
-      LL_I2C_ClearFlag_ARLO(m_i2c_config.i2c_handle);
-      LL_I2C_ClearFlag_BERR(m_i2c_config.i2c_handle);
-      LL_I2C_ClearFlag_OVR(m_i2c_config.i2c_handle);
-      LL_I2C_ClearFlag_AF(m_i2c_config.i2c_handle);
-   }
+   void clear_status();
 
    static constexpr std::size_t max_bytes_per_i2c_transfer = 255u;
 
---
+++ common/stm/hw/src/i2c/I2c.cpp
@@ -0,0 +1,320 @@
+#include "hw/i2c/I2c.hpp"
+
+namespace hw::i2c
+{
+
+I2c::I2c(I2cConfig& config)
+    : m_i2c_config{config}
+{
+}
+
+void I2c::prepare_for_communication()
+{
+   while ((LL_I2C_IsActiveFlag_STOP(m_i2c_config.i2c_handle) == 1u) ||
+          (LL_I2C_IsActiveFlag_ARLO(m_i2c_config.i2c_handle) == 1u))   // Loop until end of transfer received
+   {
+      LL_I2C_Disable(m_i2c_config.i2c_handle);
+      clear_status();
+   }
+
+   while (LL_I2C_IsEnabled(m_i2c_config.i2c_handle) == 0u)
+   {
+      LL_I2C_Enable(m_i2c_config.i2c_handle);
+   }
+
+   m_state = I2cState::idle;
+}
+
+bool I2c::attempt_reinit()
+{
+   bool success = false;
+
+   if ((LL_I2C_IsActiveFlag_STOP(m_i2c_config.i2c_handle) == 1u) ||
+       (LL_I2C_IsActiveFlag_ARLO(m_i2c_config.i2c_handle) == 1u))
+   {
+      LL_I2C_Disable(m_i2c_config.i2c_handle);
+      clear_status();
+   }
+
+   if (LL_I2C_IsEnabled(m_i2c_config.i2c_handle) == 0u)
+   {
+      LL_I2C_Enable(m_i2c_config.i2c_handle);
+      success = true;
+      m_state = I2cState::idle;
+   }
+
+   return success;
+}
+
+bool I2c::write(const uint8_t i2c_addr, std::span<const uint8_t> tx_buffer)
+{
+   error::verify(tx_buffer.size() <= max_bytes_per_i2c_transfer);
+
+   if (m_state == I2cState::idle)
+   {
+      m_current_i2c_addr = i2c_addr;
+      m_tx_buffer        = tx_buffer;
+      m_rx_buffer        = {};
+      m_read_reg         = std::nullopt;
+      m_tx_index         = 0;
+      m_rx_index         = 0;
+
+      enable_interrupts();
+
+      m_read_transaction.store(false, std::memory_order_relaxed);
+      m_state = I2cState::start_condition_for_data;
+
+      // generate start
+      LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);
+
+      return true;
+   }
+
+   return false;
+}
+
+bool I2c::read(const uint8_t i2c_addr, std::span<uint8_t> rx_buffer, std::optional<uint8_t> reg)
+{
+   error::verify(rx_buffer.size() <= max_bytes_per_i2c_transfer);
+
+   if (m_state == I2cState::idle)
+   {
+      m_current_i2c_addr = i2c_addr;
+      m_tx_buffer        = {};
+      m_rx_buffer        = rx_buffer;
+      m_read_reg         = reg;
+      m_tx_index         = 0;
+      m_rx_index         = 0;
+
+      enable_interrupts();
+
+      m_read_transaction.store(true, std::memory_order_relaxed);
+
+      if (m_read_reg.has_value())
+      {
+         m_state = I2cState::start_condition_for_read_register;
+      }
+      else
+      {
+         m_state = I2cState::start_condition_for_data;
+      }
+
+      // generate start
+      LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);
+      return true;
+   }
+
+   return false;
+}
+
+void I2c::handle_i2c_event_interrupt()
+{
+   const auto state            = m_state;
+   const auto read_transaction = m_read_transaction.load(std::memory_order_relaxed);
+
+   switch (state)
+   {
+      case I2cState::idle:
+      case I2cState::error:
+         // do nothing
+         break;
+
+      case I2cState::start_condition_for_read_register:
+         if (LL_I2C_IsActiveFlag_SB(m_i2c_config.i2c_handle) == 1u)
+         {
+            LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) & ~0x01);
+            m_state = I2cState::addr_flag_wait_for_read_register;
+         }
+         break;
+
+      case I2cState::addr_flag_wait_for_read_register:
+         if (LL_I2C_IsActiveFlag_ADDR(m_i2c_config.i2c_handle) == 1u)
+         {
+            LL_I2C_ClearFlag_ADDR(m_i2c_config.i2c_handle);
+            m_state = I2cState::send_read_register;
+         }
+         break;
+
+      case I2cState::send_read_register:
+         if (LL_I2C_IsActiveFlag_TXE(m_i2c_config.i2c_handle) == 1u)
+         {
+            LL_I2C_TransmitData8(m_i2c_config.i2c_handle, m_read_reg.value());
+            LL_I2C_GenerateStartCondition(m_i2c_config.i2c_handle);   // re-generate start condition
+            m_state = I2cState::start_condition_for_data;
+         }
+         break;
+
+      case I2cState::start_condition_for_data:
+         if (LL_I2C_IsActiveFlag_SB(m_i2c_config.i2c_handle) == 1u)
+         {
+            if (read_transaction)
+            {
+               LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) | 0x01);
+            }
+            else
+            {
+               LL_I2C_TransmitData8(m_i2c_config.i2c_handle, static_cast<uint8_t>(m_current_i2c_addr << 1u) & ~0x01);
+            }
+
+            m_state = I2cState::addr_flag_wait_for_data;
+         }
+         break;
+
+      case I2cState::addr_flag_wait_for_data:
+         if (LL_I2C_IsActiveFlag_ADDR(m_i2c_config.i2c_handle) == 1u)
+         {
+            LL_I2C_ClearFlag_ADDR(m_i2c_config.i2c_handle);
+
+            if (read_transaction)
+            {
+               // for single-byte read, NACK is made before ADDR is cleared
+               if (m_rx_buffer.size() == 1u)
+               {
+                  LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_NACK);
+                  LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
+               }
+               else
+               {
+                  LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_ACK);
+               }
+            }
+            m_state = I2cState::data_transaction;
+         }
+         break;
+
+      case I2cState::data_transaction:
+         if (read_transaction)
+         {
+            const auto rx_length = m_rx_buffer.size();
+            if (m_rx_index < rx_length)
+            {
+               receive_byte();
+               // the ACK bit must be cleared just after reading the second last data byte
+               if (m_rx_index == rx_length - 1u)
+               {
+                  LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_NACK);
+                  LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
+               }
+               else
+               {
+                  LL_I2C_AcknowledgeNextData(m_i2c_config.i2c_handle, LL_I2C_ACK);
+               }
+            }
+
+            if (m_rx_index == rx_length)
+            {
+               stop(true);   // rx data read complete
+
+               if (m_receive_complete_callback != nullptr)
+               {
+                  m_receive_complete_callback(m_callback_context);
+               }
+            }
+         }
+         else
+         {
+            // write
+            const auto tx_length = m_tx_buffer.size();
+            if (m_tx_index < tx_length)
+            {
+               send_byte();
+            }
+
+            if (m_tx_index == tx_length)
+            {
+               if (LL_I2C_IsActiveFlag_BTF(m_i2c_config.i2c_handle))
+               {
+                  stop(true);   // tx data write complete
+               }
+            }
+         }
+         break;
+
+      default:
+         error::stop_operation();
+         break;
+   }
+}
+
+void I2c::handle_i2c_error_interrupt()
+{
+   stop(false);
+}
+
+bool I2c::has_error() const
+{
+   return (m_state == I2cState::error);
+}
+
+void I2c::register_receive_complete_callback(const ReceiveCompleteCallback callback, void* context)
+{
+   error::verify(callback != nullptr);
+   m_receive_complete_callback = callback;
+   m_callback_context          = context;
+}
+
+void I2c::stop(const bool success)
+{
+   LL_I2C_GenerateStopCondition(m_i2c_config.i2c_handle);
+   clear_status();
+
+   // clear any spurious byte lingering around
+   if (LL_I2C_IsActiveFlag_RXNE(m_i2c_config.i2c_handle) == 1u)
+   {
+      [[maybe_unused]] auto _ = LL_I2C_ReceiveData8(m_i2c_config.i2c_handle);
+   }
+
+   disable_interrupts();
+
+   if (success)
+   {
+      m_state = I2cState::idle;
+   }
+   else
+   {
+      m_state = I2cState::error;
+   }
+}
+
+void I2c::receive_byte()
+{
+   if (LL_I2C_IsActiveFlag_RXNE(m_i2c_config.i2c_handle) == 1u)
+   {
+      m_rx_buffer[m_rx_index] = LL_I2C_ReceiveData8(m_i2c_config.i2c_handle);
+      ++m_rx_index;
+   }
+}
+
+void I2c::send_byte()
+{
+   if (LL_I2C_IsActiveFlag_TXE(m_i2c_config.i2c_handle) == 1u)
+   {
+      LL_I2C_TransmitData8(m_i2c_config.i2c_handle, m_tx_buffer[m_tx_index]);
+      ++m_tx_index;
+   }
+}
+
+void I2c::enable_interrupts()
+{
+   LL_I2C_EnableIT_EVT(m_i2c_config.i2c_handle);
+   LL_I2C_EnableIT_ERR(m_i2c_config.i2c_handle);
+   LL_I2C_EnableIT_BUF(m_i2c_config.i2c_handle);
+}
+
+void I2c::disable_interrupts()
+{
+   LL_I2C_DisableIT_EVT(m_i2c_config.i2c_handle);
+   LL_I2C_DisableIT_ERR(m_i2c_config.i2c_handle);
+   LL_I2C_DisableIT_BUF(m_i2c_config.i2c_handle);
+}
+
+void I2c::clear_status()
+{
+   LL_I2C_ClearFlag_STOP(m_i2c_config.i2c_handle);
+   LL_I2C_ClearFlag_ARLO(m_i2c_config.i2c_handle);
+   LL_I2C_ClearFlag_BERR(m_i2c_config.i2c_handle);
+   LL_I2C_ClearFlag_OVR(m_i2c_config.i2c_handle);
+   LL_I2C_ClearFlag_AF(m_i2c_config.i2c_handle);
+}
+
+}   // namespace hw::i2c
