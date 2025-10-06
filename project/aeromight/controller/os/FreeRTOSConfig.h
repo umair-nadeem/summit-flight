68e53e9bed8f3ff1a46ce8b9ffff5c74f3606103
Umair Nadeem <umair.cn@outlook.com>
2025-10-06 21:04:29 +0100

    turn off rtos time slicing

--- project/aeromight/controller/os/FreeRTOSConfig.h
+++ project/aeromight/controller/os/FreeRTOSConfig.h
@@ -13,7 +13,7 @@
 #define configCPU_CLOCK_HZ    ( ( unsigned long ) SystemCoreClock )
 #define configTICK_RATE_HZ                         1000
 #define configUSE_PREEMPTION                       1
-#define configUSE_TIME_SLICING                     1
+#define configUSE_TIME_SLICING                     0
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION    0
 #define configUSE_TICKLESS_IDLE                    0
 #define configMAX_PRIORITIES                       10
